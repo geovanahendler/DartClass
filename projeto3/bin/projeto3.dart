ee5a6a2edb96c25ebe5ea0e873b250981cb63f2d
geovanahendler <geovana.hendler@gmail.com>
2025-01-08 18:47:55 -0300

    projeto 3

---
+++ projeto3/bin/projeto3.dart
@@ -0,0 +1,130 @@
+void main(List<String> arguments) {
+  Carro carro = Carro('Volks', 'Fusca', 1999, 5, 'gasolina');
+  Aviao aviao = Aviao('Gol', 'asaDelta', 2023, 200, 'etanol', comercial: true);
+  Bicicleta bici = Bicicleta('Azul', '4000', 2029, 1, 'nenhum');
+
+  carro.mover();
+  carro.abastecer();
+  carro.passageiros();
+
+  aviao.mover();
+  aviao.abastecer();
+  aviao.passageiros();
+  aviao.comercialPassageiros();
+
+  bici.mover();
+  bici.abastecer();
+  bici.passageiros();
+}
+
+class Veiculo {
+  String marca;
+  String modelo;
+  int ano;
+  int quantidaLugares;
+  String combustivel;
+
+  Veiculo(
+    this.marca,
+    this.modelo,
+    this.ano,
+    this.quantidaLugares,
+    this.combustivel,
+  );
+}
+
+class Carro extends Veiculo implements Veiculos {
+  Carro(
+    String marca,
+    String modelo,
+    int ano,
+    int quantidadeLugares,
+    String combustivel,
+  ) : super(marca, modelo, ano, quantidadeLugares, combustivel);
+
+  @override
+  mover() {
+    print("O carro se movimenta no chão em quatro rodas.");
+  }
+
+  @override
+  void abastecer() {
+    print('Abastecendo o carro com $combustivel');
+  }
+
+  @override
+  void passageiros() {
+    print('O carro pode carregar $quantidaLugares pessoas');
+  }
+}
+
+class Aviao extends Veiculo implements Veiculos {
+  bool? comercial;
+
+  Aviao(
+    String marca,
+    String modelo,
+    int ano,
+    int quantidadeLugares,
+    String combustivel, {
+    this.comercial,
+  }) : super(marca, modelo, ano, quantidadeLugares, combustivel);
+
+  void comercialPassageiros() {
+    if (comercial == true) {
+      print('Esse é um avião comercial. Temos as classes economica e vip.');
+    } else {
+      print('Esse é um avião particular');
+    }
+  }
+
+  @override
+  mover() {
+    print("O avião se movimenta no ar atraves de suas assas.");
+  }
+
+  @override
+  void abastecer() {
+    print('Abastecendo o avião com $combustivel');
+  }
+
+  @override
+  void passageiros() {
+    print('O avião pode carregar $quantidaLugares pessoas');
+  }
+}
+
+class Bicicleta extends Veiculo implements Veiculos {
+  Bicicleta(
+    String marca,
+    String modelo,
+    int ano,
+    int quantidadeLugares,
+    String combustivel,
+  ) : super(marca, modelo, ano, quantidadeLugares, combustivel);
+
+  @override
+  mover() {
+    print('A bicicleta se movimenta em duas rodas. Rode o pedal para andar!');
+  }
+
+  @override
+  void abastecer() {
+    print(
+      'A bicicleta não precisa abastecer, apenas use a energia do seu corpo!',
+    );
+  }
+
+  @override
+  void passageiros() {
+    print(
+      'As biciletas comuns podem carregar apenas uma pessoa. Esse modelo $modelo pode carrregar $quantidaLugares pessoa(s)',
+    );
+  }
+}
+
+abstract class Veiculos {
+  void mover();
+  void abastecer();
+  void passageiros();
+}
---
+++ projeto3/test/projeto3_test.dart
@@ -0,0 +1,8 @@
+import 'package:projeto3/projeto3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
